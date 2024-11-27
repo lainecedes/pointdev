34492906760a7f42a9e80a832e0b495b174580c7
lainecedes <elaine.parieh@gmail.com>
2024-11-27 17:34:10 +0100

    Migration

--- app.js
+++ app.js
@@ -1,21 +1,55 @@
 const express = require('express');
 const app = express();
+const port = 3000;
+const path = require('path');
+const fs = require('fs');
+
+app.use(express.static(path.join(__dirname, "public")));
 
 app.set('view engine', 'ejs');
-app.use(express.static('public'));
+app.set("views", path.join(__dirname, "views"));
+
+const projectFilePath = path.join(__dirname, 'public/data/projects.json');
 
 app.get('/', (req, res) => {
-    res.render('base', {
-        title: 'Home',
-        body: '<%- include("partials/home") %>'
+    fs.readFile(projectFilePath, 'utf8', (err, data) => {
+      if (err) {
+        console.error('Error reading the projects file:', err);
+        res.status(500).send('Internal Server Error');
+        return;
+      }
+  
+      const projects = JSON.parse(data).projects;
+      res.render('base', { 
+        title: 'Home', 
+        content: 'index',  // home.ejs
+        projects
+      });
     });
-});
-
-app.get('/about', (req, res) => {
+  });
+  
+  app.get('/about', (req, res) => {
     res.render('base', {
-        title: 'About',
-        body: '<%- include("partials/about") %>'
+      title: 'About',
+      content: 'about',  // about.ejs
     });
-});
+  });
 
-app.listen(3000, () => console.log('Server running on http://localhost:3000'));
+  app.get('/archive', (req, res) => {
+    res.render('base', {
+      title: 'The Archive',
+      content: 'archive',  // archive.ejs
+    });
+  });
+
+  app.get('/ade-redesign', (req, res) => {
+    res.render('base', {
+      title: 'ADE Redesign',
+      content: 'ade',  // ade.ejs
+    });
+  });
+  
+
+app.listen(port, () => {
+  console.log(`App listening at http://localhost:${port}`);
+});
