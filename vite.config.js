c34d6b9d10956b1a1637491d903b613dd09df45c
lainecedes <elaine.parieh@gmail.com>
2024-12-27 17:11:42 +0100

    Vercel/Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,24 @@
 import { defineConfig } from 'vite';
+import path from 'node:path';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 export default defineConfig({
+    plugins: [],
     build: {
-        outDir: 'public',
-        emptyOutDir: true
+        minify: false,
+        manifest: true,
+        emptyOutDir: false,
+        outDir: path.resolve(__dirname, 'dist'), // dist directory for the vite build
+        rollupOptions: {
+            input: path.resolve(__dirname, 'public/scripts/script.js'),
+        },
+        define: {
+            'process.env.PORT': JSON.stringify(process.env.PORT),
+            'process.env.CLIENT_ID': JSON.stringify(process.env.CLIENT_ID),
+            'process.env.CLIENT_SECRET': JSON.stringify(process.env.CLIENT_SECRET),
+            'process.env.REDIRECT_URI': JSON.stringify(process.env.REDIRECT_URI)
+        },
     },
-    server: {
-        proxy: {
-            // Proxy API requests to your Express server
-            '/api': 'http://localhost:3000'
-        }
-    }
 });
